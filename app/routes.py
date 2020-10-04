25ca475e8e75b6e41c959877ee03d23b80d810aa
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2020-10-04 16:07:30 -0300

    Adding templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world!"
+    user = {'username':'Jonathan'}
+    posts = [
+        {
+            'author':{'username':'Jhon'},
+            'body':'Python é legal'
+        },
+        {
+            'author':{'username':'Tommy'},
+            'body':'A diferença entre cura e veneno é a dose'
+        },
+        {
+            'author':{'username':'Arthur'},
+            'body':'Não depende do que você fala, mas como fala'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
